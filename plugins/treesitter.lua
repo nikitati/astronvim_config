d74bb1f69cfa724f7e3b7a697d213ab84fc8e839
nikitati <nikitati@users.noreply.github.com>
2023-04-01 10:58:07 +0200

    Init configuration

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,19 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua" },
+    ensure_installed = {
+      "lua",
+      "bash",
+      "dhall",
+      "gitignore",
+      "haskell",
+      "java",
+      "json",
+      "nix",
+      "python",
+      "scala",
+      "sql",
+      "yaml",
+    },
   },
 }
