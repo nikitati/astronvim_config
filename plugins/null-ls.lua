0d8e7305078cc2d3273ed971005c70e70a1231ac
nikitati <nikitati@users.noreply.github.com>
2023-04-25 15:17:10 +0200

    Hard-code pyproject.toml in null-ls formatters

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -2,7 +2,7 @@ return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,9 +11,16 @@ return {
       -- Set a formatter
       -- null_ls.builtins.formatting.stylua,
       -- null_ls.builtins.formatting.prettier,
-      -- null_ls.builtins.formatting.black,
-      -- null_ls.builtins.formatting.ruff,
+      null_ls.builtins.formatting.black.with {
+        extra_args = function(params) return { "--config", params.root .. "/src/python/pyproject.toml" } end,
+        cwd = function(params) return params.root .. "/src/python/analytics" end,
+      },
+      null_ls.builtins.formatting.ruff.with {
+        extra_args = function(params) return { "--config", params.root .. "/src/python/pyproject.toml" } end,
+        cwd = function(params) return params.root .. "/src/python/analytics" end,
+      },
     }
+    config.debug = true
     return config -- return final config table
   end,
 }
